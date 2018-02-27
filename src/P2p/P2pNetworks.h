d500c711e1e29ca42187d9170d589dfd00e15550
Inspire <Inspire@DESKTOP-H2QRVK6>
2018-02-27 12:46:39 +0000

    Source Code

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x42, 0x55, 0x57, 0x54, 0x56, 0x42, 0x44, 0x23, 0x45, 0x45, 0x03, 0x72, 0x30, 0x51, 0x31, 0x14 } };
+}
